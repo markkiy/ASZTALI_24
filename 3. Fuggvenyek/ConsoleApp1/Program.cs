f8c14d41dc7e493d74508961e2679adf1ba93ee8
marki <homoki.mark@ckik.hu>
2024-12-06 10:36:05 +0100

    a 3 feladat megoldasa

--- 3. Fuggvenyek/ConsoleApp1/Program.cs
+++ 3. Fuggvenyek/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,37 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+
+
+        static double Kerulet(double a, double b)
+        {
+            return 2 * (a + b);
+        }
+
+        static bool Parose(int szam)
+        {
+            return szam % 2 == 0;
+        }
+
+        
+        static int Max(int[] t)
+        {
+
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine(Kerulet(3,5);
+            // F1 - Kerulet
+            Console.WriteLine(Kerulet(1.25,4));
+
+
+            //F2 - Paros e
+            Console.WriteLine(Parose(4));
+
+            //F3 - Maximum
+            Console.WriteLine(Max(new int[] { 4, 7, -3, 12, 7, 14, -1 }));
         }
     }
 }
