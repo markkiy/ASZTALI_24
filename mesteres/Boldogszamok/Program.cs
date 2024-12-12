f556ff6fef32b28ca4a6759379e02b8132c94dad
marki <homoki.mark@ckik.hu>
2024-12-12 10:35:44 +0100

    mesteres halado boldog

---
+++ mesteres/Boldogszamok/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boldogszamok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sor = Convert.ToInt32(Console.ReadLine());
+            int[] adatok = sor.Split();
+
+        }
+    }
+}
